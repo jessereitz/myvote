383234b954ca3b7e786164ff204a53dc0c510380
Jesse Reitz <jessereitz1@gmail.com>
2018-01-20 12:15:30 -0600

    Create basic account_overview view and template

--- myvote/accounts/views.py
+++ myvote/accounts/views.py
@@ -22,5 +22,17 @@ def account_overview(request):
     #   1 - Display link to change password
     #   2 - Display link to change email
     #   3 - Display link to delete account
+    if request.user.is_authenticated:
+        return render(request, 'accounts/account_overview.html')
 
-    return render(request, 'accounts/account_overview.html')
+@login_required
+def change_password(request):
+    pass
+
+@login_required
+def change_email(request):
+    pass
+
+@login_required
+def delete_account(request):
+    pass
