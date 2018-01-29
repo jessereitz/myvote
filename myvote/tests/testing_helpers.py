b885efa1f3bce074309cc23b0de3518b1ae31530
Jesse Reitz <jessereitz1@gmail.com>
2018-01-28 19:34:08 -0600

    Create a helpers py module. This is to be used throughout test suite from heron.

---
+++ myvote/tests/testing_helpers.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import User
+from myvote.models import Poll, Option, Vote
+
+# DEFAULT CONSTANTS
+USERNAME = 'testuser'
+PASSWORD = 'testpassword12'
+
+def create_test_user(username=None, password=None):
+    """
+        Creates a test user in the db. Uses given username and password or
+        default username and password constants.
+    """
+    if username:
+        username = username
+    else:
+        username = USERNAME
+    if password:
+        password = password
+    else:
+        password = PASSWORD
+    return User.objects.create_user(username=username, password=password)
+
+def create_polls(user, amount=10):
+    """
+        Populates db with a number of test polls with 2 options. If no amount
+        given, defaults to 10. MUST have 'user' supplied.
+    """
+    POLL_NAME = "test_poll"
+    OPTION_1 = 'option1'
+    OPTION_2 = 'option2'
+
+    if not user:
+        raise Exception("User not provided.")
+
+    for i in range(0, amount):
+        new_name = POLL_NAME + "_" + str(i)
+        p = Poll(owner=user, name=new_name)
+        p.save()
+
+        option_1 = Option(option_text=OPTION_1, poll=p)
+        option_1.save()
+
+        option_2 = Option(option_text=OPTION_2, poll=p)
+        option_2.save()
--- myvote/tests/views/test_myvote_explore.py
+++ myvote/tests/views/test_myvote_explore.py
@@ -3,6 +3,7 @@ from django.urls import reverse, resolve
 from django.test import TestCase
 
 from myvote.models import Poll, Option, Vote
+from tests.testing_helpers import create_test_user, create_polls
 
 # CONSTANT VALS
 USERNAME = 'testuser'
@@ -28,40 +29,40 @@ class ExploreRecentTests(TestCase):
 
 
 # HELPERS
-def create_test_user(username=None, password=None):
-    """
-        Creates a test user in the db. Uses given username and password or
-        default username and password constants.
-    """
-    if username:
-        username = username
-    else:
-        username = USERNAME
-    if password:
-        password = password
-    else:
-        password = PASSWORD
-    return User.objects.create_user(username=username, password=password)
-
-def create_polls(user, amount=10):
-    """
-        Populates db with a number of test polls with 2 options. If no amount
-        given, defaults to 10. MUST have 'user' supplied.
-    """
-    POLL_NAME = "test_poll"
-    OPTION_1 = 'option1'
-    OPTION_2 = 'option2'
-
-    if not user:
-        raise Exception("User not provided.")
-
-    for i in range(0, amount):
-        new_name = POLL_NAME + "_" + str(i)
-        p = Poll(owner=user, name=new_name)
-        p.save()
-
-        option_1 = Option(option_text=OPTION_1, poll=p)
-        option_1.save()
-
-        option_2 = Option(option_text=OPTION_2, poll=p)
-        option_2.save()
+# def create_test_user(username=None, password=None):
+#     """
+#         Creates a test user in the db. Uses given username and password or
+#         default username and password constants.
+#     """
+#     if username:
+#         username = username
+#     else:
+#         username = USERNAME
+#     if password:
+#         password = password
+#     else:
+#         password = PASSWORD
+#     return User.objects.create_user(username=username, password=password)
+#
+# def create_polls(user, amount=10):
+#     """
+#         Populates db with a number of test polls with 2 options. If no amount
+#         given, defaults to 10. MUST have 'user' supplied.
+#     """
+#     POLL_NAME = "test_poll"
+#     OPTION_1 = 'option1'
+#     OPTION_2 = 'option2'
+#
+#     if not user:
+#         raise Exception("User not provided.")
+#
+#     for i in range(0, amount):
+#         new_name = POLL_NAME + "_" + str(i)
+#         p = Poll(owner=user, name=new_name)
+#         p.save()
+#
+#         option_1 = Option(option_text=OPTION_1, poll=p)
+#         option_1.save()
+#
+#         option_2 = Option(option_text=OPTION_2, poll=p)
+#         option_2.save()
