635f44858467f48611101035b2d3667a33eeb55d
Jesse Reitz <jessereitz1@gmail.com>
2018-02-01 21:37:07 -0600

    JS for add option working.

--- myvote/static/js/add_poll_option.js
+++ myvote/static/js/add_poll_option.js
@@ -1,28 +1,13 @@
 var createPollForm = null;
 var currentOptions = [];
 var addOptionBtn = null;
-var form = null;
+
 
 function initAddButton() {
   createPollForm = document.getElementById('create_poll_form');
   currentOptions = document.querySelectorAll('.optionInput');
   addOptionBtn = document.getElementById('add_poll_option');
-}
 
-
-function OptionInput(idNum) {
-
-  this.idNum = idNum;
-
-  this.getId = function() {return this.idNum;}
-
-  this.inputHtml = function() {
-
-    return '<input type="text" maxlength="100" id="id_option'
-            + this.idNum
-            + '" name="option' + this.idNum
-            + '">'
-  }
 }
 
 function CreatePollForm(createPollForm, currentOptions) {
@@ -30,23 +15,33 @@ function CreatePollForm(createPollForm, currentOptions) {
   this.currentOptions = Array.from(currentOptions);
 
   this.addOptionInput = function() {
+    if (this.currentOptions.length >= 5) {
+      return null;
+    }
     this.currentOptions.push(this.createOptionInput(this.currentOptions.length + 1));
-    this.form.insertBefore(this.currentOptions[this.currentOptions.length - 1], this.form.lastElementChild)
+    this.form.insertBefore(this.currentOptions[this.currentOptions.length - 1].label, this.form.lastElementChild);
+    this.form.insertBefore(this.currentOptions[this.currentOptions.length - 1], this.form.lastElementChild);
   }
 
   this.createOptionInput = function(idNum) {
+    let inputLabel = document.createElement('label');
+    inputLabel.for = "id_option" + idNum;
+    inputLabel.textContent = "option " + idNum + ":";
+
     let inputEl = document.createElement('input');
     inputEl.type = "text";
     inputEl.classList.add("optionInput");
     inputEl.name = "option" + idNum;
     inputEl.id = "id_option" + idNum;
     inputEl.maxLength = 100;
+    inputEl.label = inputLabel;
+
     return inputEl;
   }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   initAddButton();
-  form = new CreatePollForm(createPollForm, currentOptions);
-  console.log(form);
+  let form = new CreatePollForm(createPollForm, currentOptions);
+  document.getElementById('add_poll_option').addEventListener('click', ()=> {form.addOptionInput();});
 });
